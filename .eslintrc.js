652c53613006a346d8d53cad6527de6ef92a77c0
Takaki <kta.it.pc@gmail.com>
2025-07-28 13:22:28 +0000

    feat: configure ESLint with TypeScript, React, and Prettier integration
    - Add comprehensive ESLint configuration for TypeScript React project
    - Include recommended rules for TypeScript, React, and React Hooks
    - Integrate Prettier for consistent code formatting
    - Enable auto-detection of React version

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    }
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
